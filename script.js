e11edd4b4935a03b01b15261866e34e3c35bf562
Birhan Kassaye <birhanyalew2019@gmail.com>
2026-03-31 21:18:07 +0300

    create function that perform operation

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ function divide(a, b) {
         return (a / b);
     }
 }
+
+let num1
+let num2
+let operator
+
+function operate(num1, operator, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    }
+    else if (operator === "-") {
+        return subtract(num1, num2);
+    }
+    else if (operator === "*") {
+        return multiply(num1, num2);
+    }
+    else if (operator === "/") {
+        return divide(num1, num2);
+    }
+    else {
+        return "Invalid operator. Please use +, -, *, or /.";
+    }
+}
